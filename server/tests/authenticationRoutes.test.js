3f2af6b89251694833cc9ee6bad809aebee41d53
Jack Goettle <41124102+jackg235@users.noreply.github.com>
2021-04-12 12:10:01 -0400

    Fixed travis... i hope

--- server/tests/authenticationRoutes.test.js
+++ server/tests/authenticationRoutes.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest')
 const mongoose = require('mongoose')
-const app = require('../index')
+const app = require('../app')
 const UserModel = require('../data_model/User')
 
 const testUserJSON = {
