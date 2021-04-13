f2b94717ded30c1bc18b36699a63b43bc13e468a
Bryan Nguyen <brng@seas.upenn.edu>
2021-04-13 06:27:58 -0400

    cleaned up explore and started streampage

--- client/src/components/user/User.jsx
+++ client/src/components/user/User.jsx
@@ -5,46 +5,23 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import CardHeader from '@material-ui/core/CardHeader';
 import Avatar from '@material-ui/core/Avatar';
-import { connect } from 'react-redux';
 import { Grid } from '@material-ui/core';
-
-const styles = {
-    card: {
-        backgroundColor: '#e3e3e3'
-    }
-}
 
 class User extends React.Component {
     render() {
-        const {
-            classes,
-            user: {
-                userImg,
-                username,
-                firstName,
-                lastName,
-                regDate,
-                followers,
-                following,
-                blocking,
-                hidden,
-            }
-        } = this.props;
-
         return (
                 <Grid item>
                     <Card variant='outlined'>
                         <CardHeader
-                            style={styles.card}
-                            avatar={<Avatar variant="rounded" src={userImg} alt={username}/>}
+                            style={{backgroundColor: '#e3e3e3'}}
+                            avatar={<Avatar variant="rounded" src={this.props.img} alt={this.props.username}/>}
                             title={
                                 <Typography
                                     component={Link}
-                                    to={`/users/${username}`}>
-                                    {username}
+                                    to={`/users/${this.props.username}`}>
+                                    {this.props.username}
                                 </Typography>} />
                     </Card>
                 </Grid>
